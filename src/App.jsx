300db6df56a0e1ae46f1042b963c5e08f6875a74
Suhaib Khan <suhaib0900@gmail.com>
2026-02-10 22:43:49 +0000

    Better Tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,55 @@
-const ImpressionTracker = (ChildComponent) => {
-  return (props) => {
-    const handleUserClick = () => {
-      console.log(console.log("User Click - " + ChildComponent.name));
-    };
-    const handleMouseEnter = () => {
-      console.log(console.log("Mouse Enter - " + ChildComponent.name));
-    };
-    const handleMouseLeave = () => {
-      console.log("Mouse Left - " + ChildComponent.name);
-    };
-    return (
-      <div
-        onMouseLeave={handleMouseLeave}
-        onClick={handleUserClick}
-        onMouseEnter={handleMouseEnter}
-      >
-        <ChildComponent {...props} />
-      </div>
-    );
+// const ImpressionTracker = (ChildComponent) => {
+//   return (props) => {
+// const handleUserClick = () => {
+//   console.log(console.log("User Click - " + ChildComponent.name));
+// };
+// const handleMouseEnter = () => {
+//   console.log(console.log("Mouse Enter - " + ChildComponent.name));
+// };
+// const handleMouseLeave = () => {
+//   console.log("Mouse Left - " + ChildComponent.name);
+// };
+//     return (
+//       <div
+//         onMouseLeave={handleMouseLeave}
+//         onClick={handleUserClick}
+//         onMouseEnter={handleMouseEnter}
+//       >
+//         <ChildComponent {...props} />
+//       </div>
+//     );
+//   };
+// };
+const MouseLeaveTracker = ({ children }) => {
+  const handleMouseLeave = () => {
+    console.log("Mouse Left - " + ChildComponent.name);
   };
+  return <div onMouseLeave={handleMouseLeave}>{children}</div>;
 };
-
+const MouseEnterTracker = ({ children }) => {
+  const handleMouseEnter = () => {
+    console.log(console.log("Mouse Enter - " + children.name));
+  };
+  return <div onMouseEnter={handleMouseEnter}>{children}</div>;
+};
+const ClickTracker = ({ children }) => {
+  const handleUserClick = () => {
+    console.log("User Click - " + children.name);
+  };
+  return <div onClick={handleUserClick}>{children}</div>;
+};
 const SimpleComponent = ({ text }) => {
   return <button>{text}</button>;
 };
 const App = () => {
-  const WrappedComponent = ImpressionTracker(SimpleComponent);
-  return <WrappedComponent text="Buy Now!"/>;
+  return (
+    <MouseLeaveTracker>
+      <MouseEnterTracker>
+        <ClickTracker>
+          <SimpleComponent text="Buy Now" />
+        </ClickTracker>
+      </MouseEnterTracker>
+    </MouseLeaveTracker>
+  );
 };
 export default App;
